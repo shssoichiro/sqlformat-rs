f4747fb71f48a5b918793873cbc646d189ef898d
Luca Barbato <lu_zero@gentoo.org>
2025-10-06 16:18:23 +0200

    Consider also number for tokens before format specifiers

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -157,6 +157,7 @@ fn get_type_specifier_token<'i>(
             TokenKind::Placeholder,
             TokenKind::Reserved,
             TokenKind::String,
+            TokenKind::Number,
             TokenKind::TypeSpecifier,
             TokenKind::Word,
         ]
